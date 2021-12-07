a5d3af263f7c00fabc529fe2a74f1015a2c9275b
JhormanMera <1010159945@u.icesi.edu.co>
2021-12-07 13:06:24 -0500

    Links added

--- src/RGBTransmission/RGBTransmission.ino
+++ src/RGBTransmission/RGBTransmission.ino
@@ -7,7 +7,19 @@ char currentLetter=' ';
 char compareLetter=' ';
 String binaryValue;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
-
+/*
+ * Tomamos ideas de:
+ * https://github.com/systembolaget/Physical-computing-sensor-servo-tutorial-6a-Colour-finder-with-ams-TCS34725-and-HD-1900A
+ * https://www.youtube.com/watch?v=FQnzRW4XukA
+ * https://www.youtube.com/watch?v=TWKmbTITEes
+ * https://create.arduino.cc/editor/ProfeGarcia/3e920fea-36c8-4fbc-a3a6-783f5ffa1902/preview
+ * https://create.arduino.cc/editor/ProfeGarcia/f8bf6115-9715-4345-88f2-9c3cb1ecefca/preview
+ * Bibliografía de conceptos:
+ * https://en.wikipedia.org/wiki/Color_difference
+ * https://www.calculadoraconversor.com/ascii-a-binario/
+ * Calibracion sensor
+ * https://learn.adafruit.com/calibrating-sensors/two-point-calibration
+ */
 void setup(){
  Serial.begin(9600);
  pinMode(OUTPUTLEDR,OUTPUT); // los pins digitales serán de salida
