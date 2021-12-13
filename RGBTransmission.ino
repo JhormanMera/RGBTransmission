d49e91eb502e2085615df9fbd008b71e6487410a
JhormanMera <1010159945@u.icesi.edu.co>
2021-12-13 04:17:16 -0500

    Ready

--- RGBTransmission.ino
+++ RGBTransmission.ino
@@ -1,22 +1,3 @@
-/*
- * Tomamos ideas de:
- * https://github.com/systembolaget/Physical-computing-sensor-servo-tutorial-6a-Colour-finder-with-ams-TCS34725-and-HD-1900A
- * https://www.youtube.com/watch?v=FQnzRW4XukA
- * https://www.youtube.com/watch?v=TWKmbTITEes
- * https://create.arduino.cc/editor/ProfeGarcia/3e920fea-36c8-4fbc-a3a6-783f5ffa1902/preview
- * https://create.arduino.cc/editor/ProfeGarcia/f8bf6115-9715-4345-88f2-9c3cb1ecefca/preview
- * https://www.youtube.com/watch?v=PPsUHX6nVow (Video ficheros)
- * https://forum.arduino.cc/t/converting-binary-strings-to-bytes-integers/212848/5 (String to number)
- * https://forum.arduino.cc/t/libreria-decimal-a-binario-y-binario-a-decimal/394353/3
- * https://github.com/SaadOjo/DIY_Li-Fi
- * 
- * Bibliografía de conceptos:
- * https://en.wikipedia.org/wiki/Color_difference
- * https://www.calculadoraconversor.com/ascii-a-binario/
- * Calibracion sensor
- * https://learn.adafruit.com/calibrating-sensors/two-point-calibration
- * https://www.youtube.com/watch?v=X4RevYjBJCU&t=176s
- */
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 #include "ColorConverterLib.h"
